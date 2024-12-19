bc2a472975bc715e3354b72842176bae4c069a0d
sanjaysarath <sarathkumarsaminathan@gmail.com>
2024-12-19 18:40:45 +0530

    first commit

---
+++ mai.js
@@ -0,0 +1,19 @@
+var a = "sarath";
+var b ="";
+for (var i = a.length-1; i >= 0; i--) {
+   
+    b+=a[i];
+}
+if(a===b){
+    console.log("it is a palindrome")
+}
+else{
+    console.log("it is  not  a palindrome")
+
+}
+console.log(b)//reverse a string
+var f=[1,2,3,4,5,6];
+console.log(f);
+f[0]=20;
+console.log(f);
+console.log();
